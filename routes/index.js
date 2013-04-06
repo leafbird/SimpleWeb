90161e1d2df1f9ef4090cf2d6f20442657590267
leafbird <florist.sk@gmail.com>
2013-04-06 21:48:33 +0900

    sublime 프로젝트 파일 등록.

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ twit
           process.exit(1);
       }
   })
-  .get(' statuses/user_timeline', { screen_name: 'kjcksk' }, function (err, data) {
+  .get('statuses/user_timeline', { screen_name: 'kjcksk' }, function (err, data) {
       if (err) console.log('Tweeting failed: ' + err);
       else console.log('Success!')
   }
