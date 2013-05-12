40c93a3ad119c6ce55fbc0b0b36fe7f68227ba93
sungki_choi <florist.sk@gmail.com>
2013-05-12 11:09:08 +0900

    looping and saving finished.

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,64 @@ exports.index = function (req, res) {
 
 exports.backup = function (req, res ) {
 
+	function saveStatuses(data, since_id, maxCount) {
+		dbContext.insert( 'statuses', data, function(data) {
+			console.dir( typeof data );
+
+			var smallestId = -1;
+			data.forEach( function(entry) {
+				if( smallestId < 0 )
+					smallestId = entry.id;
+				else if( entry.id < smallestId )
+					smallestId = entry.id;
+			});
+
+			console.log( 'smallestId:' + smallestId );
+
+			loopLogic( since_id, smallestId, maxCount - 1 );
+		});
+	}
+
+	function loopLogic( since_id, max_id, maxCount ) {
+
+		console.log( 'maxCount:' + maxCount );
+
+		if( maxCount == 0 ) {
+			res.send( {result:'ok'} );
+			return;
+		}
+
+		var params = {
+			include_rts: true,
+			trim_user: false,
+			//since_id:,
+			//max_id:,
+			count: 100,
+			screen_name: user_name,
+		};	
+
+		if(since_id > 0)
+			params.since_id = since_id;
+		if(max_id > 0)
+			params.max_id = max_id;
+
+		console.dir(params);
+
+		var twit = twitContext.getTwit();
+		twit.getUserTimeline( params, function(err, data) {
+			
+			if (err) throw err;
+
+			if (data.length == 0 ) {
+				res.send( {result:'no more data'} );
+				return;
+			}
+
+			console.log(format('maxCount:%d, dataLength:%d', maxCount, data.length) );
+			saveStatuses(data, since_id, maxCount);
+		});
+	}
+
 	async.waterfall([
 
 		// 
@@ -34,56 +92,8 @@ exports.backup = function (req, res ) {
 		function(latestId, cb) {
 
 			console.log(format('max_id:%d', latestId));
-
-			var twit = twitContext.getTwit();
-			var params = {
-				include_rts: true,
-				trim_user: false,
-				//since_id:,
-				//max_id:,
-				count: 100,
-				screen_name: user_name,
-			};
-			if(latestId > 0)
-				params.since_id = latestId;
-
-			var loopLogic = function( params, maxCount ) {
-
-				console.log( 'maxCount:' + maxCount );
-
-				if( maxCount == 0 )
-					return;
-
-				console.dir(params);
-				twit.getUserTimeline( params, function(err, data) {
-					
-					if (err) throw err;
-
-					if (data.length == 0 ) {
-						res.send( {result:'no more data'} );
-						return;
-					}
-
-					console.log(format('maxCount:%d, dataLength:%d', maxCount, data.length) );
-					cb(null, data);
-
-					var smallestId = -1;
-					data.forEach( function(entry) {
-						if( smallestId < 0 )
-							smallestId = entry.id;
-						else if( entry.id < smallestId )
-							smallestId = entry.id;
-					});
-
-					console.log( 'smallestId:' + smallestId );
-					params.max_id = smallestId;
-
-					loopLogic( params, maxCount - 1 );
-				});
-			}
-
-			loopLogic( params, 3 );
-
+			
+			loopLogic( latestId, -1, 3 );
 		},
 
 		function(data, cb) {
