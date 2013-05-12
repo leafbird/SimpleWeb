f882bc0eb9d04001bbc7ed427128a4b02e809f01
sungki_choi <florist.sk@gmail.com>
2013-05-12 20:40:26 +0900

    count 획득 방식을 비동기로 변경.

--- modules/dbContext.js
+++ modules/dbContext.js
@@ -7,71 +7,94 @@
 //require node modules (see package.json)
 var MongoClient = require('mongodb').MongoClient
     , format = require('util').format
-    , async = require('async');
-
-var collectionNames = [ 'statuses', 'favorites' ];
+    ;
 
 var collections = {};
-var counts = {};
+var arrCollection = [];
 
 exports.init = function(user_name) {
 
-	async.waterfall([
-		function(cb){
-			//connect away
-			MongoClient.connect('mongodb://localhost:27017/twitter', function (err, db) {
+	//connect away
+	MongoClient.connect('mongodb://localhost:27017/twitter', function (err, db) {
 
-			    if (err) throw err;
+		if (err) throw err;
 
-			    console.log("Connected to Database");
+	    console.log("Connected to Database");
 
-			    for (var i = 0; i < collectionNames.length; i++) {
+		['statuses', 'favorites'].forEach( function( name ) {
+			var value = {};
+	    	value.name = name;
 
-			    	var name = collectionNames[i];
+	    	console.log(format('dbg] coll name : %s', name))
 
-			    	console.log(format('dbg] coll name : %s', name))
-
-			    	collections[name] = db.collection(format('%s_%s', name, user_name) );
-
-			    	cb(null, collections[name], name);
-			    }
-			});
-		},
-
-		function(collection, name, cb) {
-
-		    collection.count(function (err, count) {
-
-		        console.log(format('%s size : %d', name, count));
-
-		        counts[name] = count;
-		    });
-		}
-	])
-
+	    	value.cursor = db.collection(format('%s_%s', name, user_name) );
+	    	arrCollection.push(value)
+	    	collections[name] = value;
+		});
+	});
 }
 
-exports.getCounts = function() {
+function asyncMap( list, fn, cb_ ) {
+	var n = list.length
+		, result = []
+    	, errState = null;
+    
+  	function cb (er, data) {
+    	if (errState) return;
+    	if (er) return cb_(errState = er);
+    	results.push(data);
+    	if (--n === 0) // 모든 리스트 처리 완료시
+      		return cb_(null, results);
+  	}
+  
+  	// action code
+  	list.forEach(function (l) {
+    	fn(l, cb);
+  	});
+}
 
-	console.dir( counts );
+function asyncObjMap( list, fn, cb_ ) {
+	var n = list.length
+		, result = {} 
+    	, errState = null;
+    
+  	function cb (er, key, value) {
+    	if (errState) return;
+    	if (er) return cb_(errState = er);
+    	result[key] = value;
+    	if (--n === 0) // 모든 리스트 처리 완료시
+      		return cb_(null, result);
+  	}
+  
+  	// action code
+  	list.forEach(function (l) {
+    	fn(l, cb);
+  	});
+}
+      
+exports.getCounts = function( callback ) {
 
-	return counts;
+	asyncObjMap( arrCollection,
+		function( data, cb ) {
+			data.cursor.count( function (err, count) {
+				cb( null, data.name, count );
+			});
+		}, callback );
 }
 
 exports.getLatestId = function( name, callback ) {
-	var coll = collections[ name ];
+	var coll = collections[name].cursor;
 
 	coll.findOne( {}, { sort: { id: -1 } , fields: { id: 1 } }, function( err, doc ) {
 		if( err ) throw err;
 
-		console.log( 'doc:'+doc );
-		var id = doc ? doc.id : -1;
-		callback( id );
+		console.log( 'doc:'+ JSON.stringify(doc) );
+		callback( doc ? doc.id : -1 );
 	} );
 }
 
 exports.insert = function( name, data, callback ) {
-	var coll = collections[ name ];
+	var coll = collections[name].cursor;
 
 	coll.insert( data, { safe: true }, function( err, doc ) {
 		if(err) throw err;
--- routes/index.js
+++ routes/index.js
@@ -13,10 +13,17 @@ twitContext.init(user_name);
 dbContext.init(user_name);
 
 exports.index = function (req, res) {
-    res.render('index.html', { 
-    	user: twitContext.user_data(),
-    	dbCounts: dbContext.getCounts(),
-    });
+
+	dbContext.getCounts( function(err, result) {
+
+		console.dir( result );
+
+	    res.render('index.html', { 
+	    	user: twitContext.user_data(),
+	    	dbCounts: result,
+	    });
+
+	});
 };
 
 exports.backup = function (req, res ) {
