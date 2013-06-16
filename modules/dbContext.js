6ca0c1b8b9e9f7ce437fda03ba33a30cad15b281
sungki_choi <florist.sk@gmail.com>
2013-06-16 15:27:45 +0900

    월별 트윗 개수 쿼리 작성 완료. view 만들어야 함.

--- modules/dbContext.js
+++ modules/dbContext.js
@@ -94,6 +94,7 @@ exports.groupBySource = function( callback ) {
 			throw err;
 
 		// docs = { key1: value1, key2: value2, ... }
+		// key : source, value : count.
 		docs = docs.reduce( function( previousValue, currentValue ) {
 			if( previousValue[ currentValue.source ] )
 				previousValue[ currentValue.source ]++;
@@ -120,6 +121,33 @@ exports.groupBySource = function( callback ) {
 	});
 }
 
+Date.prototype.format = function(f) {
+    if (!this.valueOf()) return " ";
+ 
+    var weekName = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
+    var d = this;
+     
+    return f.replace(/(yyyy|yy|MM|dd|E|hh|mm|ss|a\/p)/gi, function($1) {
+        switch ($1) {
+            case "yyyy": return d.getFullYear();
+            case "yy": return (d.getFullYear() % 1000).zf(2);
+            case "MM": return (d.getMonth() + 1).zf(2);
+            case "dd": return d.getDate().zf(2);
+            case "E": return weekName[d.getDay()];
+            case "HH": return d.getHours().zf(2);
+            case "hh": return ((h = d.getHours() % 12) ? h : 12).zf(2);
+            case "mm": return d.getMinutes().zf(2);
+            case "ss": return d.getSeconds().zf(2);
+            case "a/p": return d.getHours() < 12 ? "오전" : "오후";
+            default: return $1;
+        }
+    });
+};
+ 
+String.prototype.string = function(len){var s = '', i = 0; while (i++ < len) { s += this; } return s;};
+String.prototype.zf = function(len){return "0".string(len - this.length) + this;};
+Number.prototype.zf = function(len){return this.toString().zf(len);};
+
 exports.groupByDate = function( callback ) {
 
 	if( data.groupByDate ) {
@@ -128,4 +156,35 @@ exports.groupByDate = function( callback ) {
 	}
 
 	var coll = collections['statuses'].cursor;
+
+	coll.find( {}, {created_at:true} ).toArray( function( err, docs ) {
+
+		if( err )
+			throw err;
+
+		// docs = { key1: value1, key2: value2, ... }
+		// key : 'yyyy.dd', value : count
+		docs = docs.reduce( function( previousValue, currentValue ) {
+			var strDateKey = new Date( currentValue.created_at ).format( 'yyyy.MM' );
+			if( previousValue[ strDateKey ] )
+				previousValue[ strDateKey ]++;
+			else
+				previousValue[ strDateKey ] = 1;
+
+			return previousValue;
+		}, {});
+
+		// arr = [ [key1, value1], [key2, value2], ... ]
+		var arr = [];
+		for( key in docs ) {
+			arr.push( [key, docs[key] ] );
+		}
+
+		arr.sort( function( a, b ) { return b[0] - a[0]; } );
+		console.dir( arr );
+
+		callback( null, arr );
+		
+		data.groupBySource = arr;
+	});
 }
